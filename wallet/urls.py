54613d63de35b977866ac7ce4bc5de86f2b3f5af
Nyakuar <nyakuaryy@gmail.com>
2022-09-08 17:45:07 +0300

    update

--- wallet/urls.py
+++ wallet/urls.py
@@ -4,14 +4,16 @@ from django.urls import path
 from.views import register_card, register_customer, register_notifications,register_transaction, register_wallet,register_reward,register_receipt,register_loan,register_account,register_thirdparty,register_currency
 
 
-urlpatterns=[path("register/",register_customer,name="registration")]
-urlpatterns=[path("register/",register_wallet,name="wallet")]
-urlpatterns=[path("register/",register_account,name="account")]
-urlpatterns=[path("register/",register_card,name="card")]
-urlpatterns=[path("register/",register_thirdparty,name="thirdparty")]
-urlpatterns=[path("register/",register_notifications,name="notifications")]
-urlpatterns=[path("register/",register_receipt,name="receipts")]
-urlpatterns=[path("register/",register_loan,name="loan")]
-urlpatterns=[path("register/",register_reward,name="reward")]
-urlpatterns=[path("register/",register_transaction,name="transaction")]
-urlpatterns=[path("register/",register_currency,name="currency")    ]
+urlpatterns=[
+    path("registercustomer/",register_customer,name="Customer"),
+    path("registerwallet/",register_wallet,name="wallet"),
+    path("registeraccount/",register_account,name="account"),
+    path("registercard/",register_card,name="card"),
+    path("registerthirdparty/",register_thirdparty,name="thirdparty"),
+    path("registernotify/",register_notifications,name="notifications"),
+    path("registerreceipts/",register_receipt,name="receipts"),
+    path("registerloan/",register_loan,name="loan"),
+    path("registerreward/",register_reward,name="reward"),
+    path("registertransact/",register_transaction,name="transaction"),
+    path("registercurrency/",register_currency,name="currency")
+]
